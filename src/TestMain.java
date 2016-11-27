2f00e79be30b034a52581c8019efca7329e36f31
migafgarcia <migafgarcia@gmail.com>
2016-11-27 01:31:51 +0000

    Update TestMain.java

--- src/TestMain.java
+++ src/TestMain.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 public class TestMain {
 
     public static void main(String[] args) {
-        byte[] test = {1,2};
-        System.out.println(BitUtils.byteArrayToInt(test));
 
-        byte test2 = 1;
-        System.out.println(Arrays.toString(BitUtils.byteToBooleanArray(test2)));
+        byte test2 = 13;
+
+        boolean[] booleans = BitUtils.byteToBooleanArray(test2);
+        System.out.println(Arrays.toString(booleans));
+
+        System.out.println(BitUtils.booleanArrayToByte(booleans));
+
 
 
     }
