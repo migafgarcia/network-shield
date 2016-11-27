e8c8432242b076663f765e1d0261d3fa90e862f0
migafgarcia <migafgarcia@gmail.com>
2016-11-27 01:32:33 +0000

    Update BitUtils.java
    Changed byte to boolean array method and added the reverse method

--- src/utils/BitUtils.java
+++ src/utils/BitUtils.java
@@ -20,16 +20,31 @@ public class BitUtils {
      * @return
      */
     public static boolean[] byteToBooleanArray(byte _byte) {
+        //bit = (number >> x) & 1;
         boolean[] booleanArray = new boolean[8];
-        booleanArray[7] = ((_byte & 0x01) != 0);
-        booleanArray[6] = ((_byte & 0x02) != 0);
-        booleanArray[5] = ((_byte & 0x04) != 0);
-        booleanArray[4] = ((_byte & 0x08) != 0);
-        booleanArray[3] = ((_byte & 0x16) != 0);
-        booleanArray[2] = ((_byte & 0x32) != 0);
-        booleanArray[1] = ((_byte & 0x64) != 0);
-        booleanArray[0] = ((_byte & 0x128) != 0);
+        booleanArray[7] = ((_byte >> 0) & 1) != 0;
+        booleanArray[6] = ((_byte >> 1) & 1) != 0;
+        booleanArray[5] = ((_byte >> 2) & 1) != 0;
+        booleanArray[4] = ((_byte >> 3) & 1) != 0;
+        booleanArray[3] = ((_byte >> 4) & 1) != 0;
+        booleanArray[2] = ((_byte >> 5) & 1) != 0;
+        booleanArray[1] = ((_byte >> 6) & 1) != 0;
+        booleanArray[0] = ((_byte >> 7) & 1) != 0;
         return booleanArray;
+    }
+
+    public static byte booleanArrayToByte(boolean[] booleanArray) {
+        byte _byte = 0;
+        _byte |= (booleanArray[7] ? 1 : 0) << 0;
+        _byte |= (booleanArray[6] ? 1 : 0) << 1;
+        _byte |= (booleanArray[5] ? 1 : 0) << 2;
+        _byte |= (booleanArray[4] ? 1 : 0) << 3;
+        _byte |= (booleanArray[3] ? 1 : 0) << 4;
+        _byte |= (booleanArray[2] ? 1 : 0) << 5;
+        _byte |= (booleanArray[1] ? 1 : 0) << 6;
+        _byte |= (booleanArray[0] ? 1 : 0) << 7;
+        return _byte;
+
     }
 
     /**
