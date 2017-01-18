d151702af368f9ea29137b2446cb992213379e7c
migafgarcia <migafgarcia@gmail.com>
2017-01-18 18:40:11 +0000

    Added testing

---
+++ src/test/java/DnsParsingTest.java
@@ -0,0 +1,15 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class DnsParsingTest {
+    @Test
+    public void evaluatesExpression() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void evaluatesExpression2() {
+        assertEquals(1,1);
+    }
+}
